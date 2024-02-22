8c83b4db6cc730f58aceceafc7e4755fff834621
GitHub Action <action@github.com>
2024-02-22 10:35:49 +0800

    cpu patch try

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,4 +23,4 @@ echo 'src-git smoothwan https://github.com/SmoothWAN/SmoothWAN-feeds' >>feeds.co
 echo 'src-git fancontrol https://github.com/JiaY-shi/fancontrol' >>feeds.conf.default
 # fix cpu_opp_table
 sed -i '49s/0x3/0xf/;56s/0x3/0xf/;63s/0x1/0xf/;' target/linux/qualcommax/patches-6.1/0054-v6.8-arm64-dts-qcom-ipq6018-use-CPUFreq-NVMEM.patch
-
+sed -i '39s/0x3/0xf/;47s/0x3/0xf/;55s/0x1/0xf/;' target/linux/qualcommax/patches-6.1/0910-arm64-dts-qcom-ipq6018-change-voltage-to-perf-levels.patch
