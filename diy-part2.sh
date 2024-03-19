e51e10d383d40d7526dd6e5d070beb0fa5b64998
GitHub Action <action@github.com>
2024-03-19 17:04:47 +0800

    try fixing gn by g++-10

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,4 +19,4 @@ sed -i '/strongswan-mod-kdf/d' package/feeds/packages/strongswan/Makefile
 # adjust fan speed
 sed -i 's/define\ MAX_TEMP\ 120/define\ MAX_TEMP\ 83/g' package/feeds/kiddin9/fancontrol/src/fancontrol.c
 # fix gn
-sed -i '/define Host\/Compile/a HOST_MAKE_OPTS += CXXFLAGS="$(TARGET_CXXFLAGS) -std=c++2a"' package/feeds/kiddin9/gn/Makefile
+sed -i '/+$(NINJA) -C $(HOST_BUILD_DIR)\/out/s/$/ CXX=g++-10/' package/feeds/kiddin9/gn/Makefile
